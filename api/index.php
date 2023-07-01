c0bddac0387ce8ec62e568af09e6f3cf6619575c
erykscarabellodias <erykscarabello97@gmail.com>
2023-07-01 11:17:26 -0300

    feat: iniciando projeto

---
+++ api/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$endereco = $_SERVER['PATH_INFO'];
+
+$rotas = require __DIR__ . '/../src/backend/rotas.php';
+
+if (!array_key_exists($endereco, $rotas)) {
+    http_response_code(404);
+
+    exit();
+}
+
+$controladorSelecionado = $rotas[$endereco];
+$controller = new $controladorSelecionado;
+$controller->processarRequisicao();
